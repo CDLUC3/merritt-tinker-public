ee24eb03b0bd45ebbc48efcf189a688a2ecfef09
Terry Brady <terrywbrady@gmail.com>
2024-03-27 19:10:24 -0700

    Create Test.java

---
+++ src/main/java/org/my/Test.java
@@ -0,0 +1,14 @@
+package org.my;
+
+public class Test {
+  public static void main(String[] argv) throws IOException {
+    new Test().hello();
+  }
+
+  publc Test() {
+  }
+
+  public void hello() {
+    System.out.println("hello");
+  }
+}
